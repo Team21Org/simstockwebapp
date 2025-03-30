e9b5aec01faa8cd01ef5c4412f7c52dc18f193ec
samuelklx <156188400+samuelklx@users.noreply.github.com>
2025-03-29 23:40:17 -0400

    modified:   app/admin/changeschedule/page.tsx

--- app/admin/changeschedule/page.tsx
+++ app/admin/changeschedule/page.tsx
@@ -6,7 +6,7 @@ export default async function ChangeSchedule() {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>Stock Sim | Market</title>
