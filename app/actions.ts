b877c168f27771bdc4cf95f46ad556a9d0687f84
samuelklx <156188400+samuelklx@users.noreply.github.com>
2025-03-30 23:11:47 -0400

    modified:   app/actions.ts 	modified:   package.json 	modified:   pnpm-lock.yaml 	deleted:    prisma/migrations/0_init/migration.sql 	new file:   prisma/migrations/20250331025502_init/migration.sql 	new file:   prisma/migrations/migration_lock.toml 	modified:   prisma/schema.prisma

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,10 @@
 // app/actions.ts
 "use server";
 import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
 
 export async function getData() {
+  dotenv.config();
   const sql = neon(process.env.DATABASE_URL);
   const data = await sql`...`;
   return data;
