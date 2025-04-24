2c214cd6928ab16f961043d41d639e118a77bdc6
jinxiphos <jlajoie@asu.edu>
2025-04-24 16:31:58 -0600

    Update page.tsx

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -5,9 +5,6 @@
 
 import prisma from "../lib/prisma";
 import Head from "next/head";
-import { revalidatePath } from "next/cache";
-
-import { redirect } from "next/navigation";
 
 export default async function CreateStock() {
 
