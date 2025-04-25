31389236b2ea34e703208aee52f5dbef359377dd
jinxiphos <jlajoie@asu.edu>
2025-04-25 14:46:00 -0600

    Update page.tsx

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -5,10 +5,8 @@
 
 import prisma from "../lib/prisma";
 import Head from "next/head";
-import { revalidatePath } from "next/cache";
 import { auth } from "../../auth";
 
-import { redirect } from "next/navigation";
 
 export default async function CreateStock() {
 
@@ -28,66 +26,15 @@ export default async function CreateStock() {
         "use server";
         const ticker = formData.get("Stock Ticker") as string;
         const companyName = formData.get("Company Name") as string;
-  
         const initialVolume = parseInt(formData.get("Daily Volume") as string);
         const openPrice = parseFloat(formData.get("Open Price") as string);
   
-        await prisma.stock.create({
-          data: {
-            stockId: Math.floor(Math.random() * 1000000),
-            ticker,
-            companyName,
-            initialVolume,
-            openPrice,
-          },
-        });
-  
-        revalidatePath("/market");
-        redirect("/market");
       }
   
       const stocks = await prisma.stock.findMany();
   
       content = (
-        <>
-          <form action={createStock} id="addStockForm">
-            <h1 id="addStockTxt">Add New Stock</h1>
-            <label htmlFor="Stock Ticker">Stock Ticker</label>
-            <input
-              type="text"
-              name="Stock Ticker"
-              id="Stock Ticker"
-              required
-              className="border border-gray-300 p-2 mb-4 w-full"
-            />
-            <label htmlFor="Company Name">Company Name</label>
-            <input
-              type="text"
-              name="Company Name"
-              id="Company Name"
-              required
-              className="border border-gray-300 p-2 mb-4 w-full"
-            />
-            <label htmlFor="Daily Volume">Daily Volume</label>
-            <input
-              type="number"
-              name="Daily Volume"
-              id="Daily Volume"
-              required
-              className="border border-gray-300 p-2 mb-4 w-full"
-            />
-            <label htmlFor="Open Price">Open Price</label>
-            <input
-              type="number"
-              name="Open Price"
-              id="Open Price"
-              step="0.01"
-              required
-              className="border border-gray-300 p-2 mb-4 w-full"
-            />
-            <button type="submit">Add Item</button>
-          </form>
-  
+         <>
           <div className="w-full max-w-md">
             <h2 id="stockList">Stock List</h2>
             <table id="stockTable" border={1} cellPadding={8}>
