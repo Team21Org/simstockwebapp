3c3f72acce5f95285e17149f39d5c6a5908fa071
samuelklx <156188400+samuelklx@users.noreply.github.com>
2025-04-26 16:46:51 -0400

    modified:   app/market/schedule/page.tsx

--- app/market/schedule/page.tsx
+++ app/market/schedule/page.tsx
@@ -1,3 +1,5 @@
+// ./app/market/schedule/page.tsx
+
 import Head from "next/head";
 import prisma from "../../lib/prisma";
 
@@ -16,24 +18,26 @@ export default async function Viewschedule() {
         <h1 id="scheduleTitle"> Current Schedule </h1>
         {marketSchedule ? (
           <p id="scheduleTxt">
-          Opening Time: {marketSchedule.startTime} <br />
-          Closing Time: {marketSchedule.endTime} <br />
-          Open Monday - Friday
-        </p>
-      ) : (
-        <p>No schedule found.</p>
-      )}
+            Opening Time: {marketSchedule.startTime} <br />
+            Closing Time: {marketSchedule.endTime} <br />
+            Open Monday - Friday
+          </p>
+        ) : (
+          <p>No schedule found.</p>
+        )}
       </div>
       <div>
         <h1 id="holidayTitle">Observed Holidays</h1>
         <div className="holidayContainer">
-        {marketSchedule?.holiday?.length > 0 ? (
-          marketSchedule.holiday.map((holiday, index) => (
-            <p id="holidayTxt" key={index}>{holiday}</p>
-          ))
-        ) : (
-          <p>No holidays found.</p>
-        )}
+          {marketSchedule?.holiday?.length > 0 ? (
+            marketSchedule.holiday.map((holiday, index) => (
+              <p id="holidayTxt" key={index}>
+                {holiday}
+              </p>
+            ))
+          ) : (
+            <p>No holidays found.</p>
+          )}
         </div>
       </div>
     </>
