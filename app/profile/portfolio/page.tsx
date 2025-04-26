3e7ec3892a0d630ea8a42c8d99650e4e8edee3f6
jinxiphos <jlajoie@asu.edu>
2025-04-25 18:35:28 -0600

    Update page.tsx

--- app/profile/portfolio/page.tsx
+++ app/profile/portfolio/page.tsx
@@ -1,51 +1,54 @@
 import Head from "next/head";
-import { auth } from "../../../auth";
+import Link from "next/link";
 
-export default async function Portfolio() {
-  const session = await auth();
-  
-    let content;
-
-    if (!session?.user?.email) {
-      content = (
-        <div>
-          <h1>You must be logged in to view this page.</h1>
-        </div>
-      );
-    } else {
-      content = (
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Stock Name</th>
-                <th>Ticker Number</th>
-                <th>Quantity Owned</th>
-                <th>Purchase Price</th>
-                <th>Purchase Date</th>
-                <th>Daily High</th>
-                <th>Daily Low</th>
-                <th>Opening Price</th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
-          <p>Account Balance</p>
-        </div>
-      );
-    }
-
+export default function Portfolio() {
   return (
     <>
       <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Stock Sim | Portfolio</title>
       </Head>
+      <div>
         <h3>Portfolio</h3>
-      {content}
+        <table>
+          <thead>
+            <tr>
+              <th>Stock Name</th>
+              <th>Ticker</th>
+              <th>Quantity Owned</th>
+              <th>Average Cost</th>
+              <th>Opening Price</th>
+              <th>Trade</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+        <br />
+        <h3>Transaction History</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Transaction Type</th>
+              <th>Amount</th>
+              <th>Ticker Number</th>
+              <th>Quantity</th>
+              <th>Transaction Date</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+        <h3>Balance</h3>
+        <p>Current Balance:</p>
+        <div>
+          <Link className="btn" href="./balance">
+            Access Balance
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
