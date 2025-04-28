f37bcbf08e06267c09352d9b83f92fe3b2d615b4
samuelklx <156188400+samuelklx@users.noreply.github.com>
2025-04-27 23:41:37 -0400

    modified:   app/lib/prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { withOptimize } from "@prisma/extension-optimize";
 
-const prisma = new PrismaClient()
-  .$extends(withAccelerate())
-  .$extends(
-    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY || "default api key" })
-  );
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
